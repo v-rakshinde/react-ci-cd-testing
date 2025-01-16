3e9f2eba6e517aef4a4892edfbc9bd18dca262ea
Rakshit Shinde <rakshit.shinde@xoriant.com>
2025-01-17 00:46:46 +0530

    Changes for dev endpoint support

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 
 function App() {
 
+  console.log(import.meta.env.VITE_BASE_URL);
   return (
     <>
       React CI/CD Testing Completed...!
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' 
-    ? '/react-ci-cd-testing/'  // Production base URL
-    : process.env.BRANCH_NAME === 'dev' 
-    ? '/react-ci-cd-testing/dev/'  // Development base URL
-    : '/react-ci-cd-testing/staging/',  // Staging base URL
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.VITE_BASE_URL || '/react-ci-cd-testing/',
+// })
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log('Current mode:', mode); // This will show the current mode during the build process
+
+  // Log the VITE_BASE_URL based on the current environment
+  console.log('VITE_BASE_URL:', env.VITE_BASE_URL);
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL || '/react-ci-cd-testing/', // Default to /react-ci-cd-testing if VITE_BASE_URL is not set
+  };
+});
